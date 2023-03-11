61e4cbccc99ee2786cfb097e8a4cd97ed060f773
AndreeTv <iorioandreaivrea@gmail.com>
2023-03-11 13:28:23 +0100

    terzo commit il bottone funziona "on/off"

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
+// BOTTONI ON/OFF
 function clickBottone() {
     for (let section of document.querySelectorAll('section')) {
         section.children[0].classList.add('collapse');
-        section.children[0].classList.add('show');
+        section.children[0].classList.add('evento');
         section.innerHTML = `
-        <button class="bottone btn btn-outline-light toggle-btn" data-bs-target="#${section.id} > div"  data-bs-toggle="collapse">ON/OFF</button>` + section.innerHTML
-        
+        <button class="bottone btn btn-outline-light toggle-btn" data-bs-target="#${section.id} > div" data-bs-toggle="collapse">ON/OFF</button>` + section.innerHTML;
     }
 }
 
@@ -16,24 +16,30 @@ function footer() {
     document.getElementById('jumbotrons').appendChild(span)[6]
 }
 
-const myModal = document.getElementById('myModal')
-const myInput = document.getElementById('myInput')
 
-myModal.addEventListener('shown.bs.modal', () => {
-  myInput.focus()
-})
 
-// carousel-inner
 function badge() {
-    let span = document.createElement('span');
-    span.classList.add('badge')
-    span.innerHTML = "Hot";
-    document.getElementById('badge').appendChild(span)[5]
+    let numeriCard =document.querySelectorAll('#badge .card-body')
+    for (let i = 0; i < numeriCard.length; i++) {
+        let badge = numeriCard[i]
+        let span = '<span style="color: blue;"> HOT </span>'
+        badge.innerHTML += sapn  
+        span.style=color
+    }
 }
 
+// CANCELLA LE CARD
+function invisibile() {
+    let card = document.querySelectorAll('.card')
+    for (let i = 0; i < card.length; i++) {
+        let rimuviCard = card[i];
+        rimuviCard.remove()
+    }
+}
 
 window.onload = function () {
     clickBottone()
     footer()
     badge()
+    
 }
